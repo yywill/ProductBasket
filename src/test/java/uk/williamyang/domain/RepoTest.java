dd4360b7a75ac2cdfa471ebc534993ec86756e10
William Yang <william.yang@bkyo.io>
2023-06-04 23:00:51 +0800

    fix junit test issues

--- src/test/java/uk/williamyang/domain/RepoTest.java
+++ src/test/java/uk/williamyang/domain/RepoTest.java
@@ -1,6 +1,6 @@
 package uk.williamyang.domain;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,7 +17,7 @@ import static org.junit.Assert.assertNotNull;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class EntityTest {
+public class RepoTest {
 
     @Autowired
     ProductRepository productRepository;
